6533821f61a9579c6272fec6b2688f2d193bc5d9
Ялосоветский Игорь <valergood@gmail.com>
2023-01-12 22:58:07 +0200

    added downloader

--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-from umqtt.simple import MQTTClient
-from picozero import pico_temp_sensor, pico_led
+import mip
+import network
+import socket
 
-# https://github.com/RaspberryPiFoundation/picozero
-def runTest():
-    pico_led.on() 
+ssid = 'IGORNET'
+password = 'IG0RNET29041971'
+
+
+
+# mip.install("umqtt.simple")
+mip.install("github:RaspberryPiFoundation/picozero/picozero/__init__.py", target="/lib/picozero")
+mip.install("github:RaspberryPiFoundation/picozero/picozero/picozero.py", target="/lib/picozero")
+# mip.install("github:iyalosovetsky/aqua/aqua.py", target="/lib/aqua")
+# mip.install("github:iyalosovetsky/aqua/boot.py", target="/lib/aqua")
+mip.install("github:iyalosovetsky/aqua/package.json")
+
+#upip.install('picozero','/lib')
+if station.isconnected() == False:
+    while station.isconnected() == False:
+      pass
+
+    print('Connection successful')
+    
+print(station.ifconfig())
+print('----------')
+
+# try:
+print("Code downloaded")
+from aqua import aqua
+mqtt = aqua.m_mqtt(station)
+mqtt.aquaProceed(station)
+
+
