454b03387a4f444dcec8de94197bad02b69a17b3
iyalosovetsky <iyalosovetsky@gmail.com>
2023-01-14 22:56:42 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,27 +1,2 @@
-# Complete project details at https://RandomNerdTutorials.com
-
-#from umqttsimple import MQTTClient
-import micropython
-import network
-#import esp
-#esp.osdebug(None)
 import gc
 gc.collect()
-from secrets import secrets
-
-ssid = secrets['ssid']
-password = secrets['password']
-
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-  pass
- 
-
-print('Connection successful',station.ifconfig())
-print('---------------------')
-
