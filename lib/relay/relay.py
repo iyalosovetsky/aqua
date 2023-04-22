44a7351f2ce8984573ce0780a8d72f7690eec726
Ihor Yalosovetskyi <ihor.yalosovetskyi@programika.com>
2023-04-22 20:14:59 +0300

    rc0/7

--- lib/relay/relay.py
+++ lib/relay/relay.py
@@ -5,10 +5,21 @@ from machine import Pin, PWM
 from secrets import topics
 import time 
 from picozero import pico_temp_sensor, pico_led
+from picozero import Speaker
 from time import ticks_ms
 import json 
 import os
 import re
+speaker = Speaker(6)
+liten_mus = [ ['d5', BEAT / 2], ['d#5', BEAT / 2], ['f5', BEAT], ['d6', BEAT], ['a#5', BEAT], ['d5', BEAT],
+              ['f5', BEAT], ['d#5', BEAT], ['d#5', BEAT], ['c5', BEAT / 2],['d5', BEAT / 2], ['d#5', BEAT],
+              ['c6', BEAT], ['a5', BEAT], ['d5', BEAT], ['g5', BEAT], ['f5', BEAT], ['f5', BEAT], ['d5', BEAT / 2],
+              ['d#5', BEAT / 2], ['f5', BEAT], ['g5', BEAT], ['a5', BEAT], ['a#5', BEAT], ['a5', BEAT], ['g5', BEAT],
+              ['g5', BEAT], ['', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT / 2], ['d6', BEAT / 2], ['c6', BEAT / 2],
+              ['a#5', BEAT / 2], ['a5', BEAT / 2], ['g5', BEAT / 2], ['a5', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT],
+              ['f5', BEAT], ['f5', BEAT], ['f5', BEAT / 2], ['d#5', BEAT / 2], ['d5', BEAT], ['f5', BEAT], ['d6', BEAT],
+              ['d6', BEAT / 2], ['c6', BEAT / 2], ['b5', BEAT], ['g5', BEAT], ['g5', BEAT], ['c6', BEAT / 2],
+              ['a#5', BEAT / 2], ['a5', BEAT], ['f5', BEAT], ['d6', BEAT], ['a5', BEAT], ['a#5', BEAT * 1.5] ]
 
 
 SWITCH_INVERSE = True
@@ -170,7 +181,8 @@ class app:
             if change and self.client is not None:    
                 self.client.publish(self.topic_pub_relay+str(i), (b'ON' if val else b'OFF'))
     
-
+    def play_liten_mus(self):
+        speaker.play(liten_mus)
     
     def client_setter(self, client):
         self.client = client
@@ -274,6 +286,8 @@ class app:
                      self.parseConfig()
                    except Exception as e:
                      print('Exception in app_cb  json load ', msg, e)
+                elif msg.upper().startswith('MUSIC') :
+                    self.play_liten_mus()
                 else :
                     print('Pico received ???',topic, msg)
                     return
